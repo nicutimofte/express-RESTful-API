d244b70d59d92862c71136de541a8adc778ea359
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-01-05 17:02:42 +0200

    wiring MongoDB and Mongoose

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 /**
  * Created by alexandrutimofte on 1/5/2017.
  */
-var express=require('express');
+var express=require('express'),
+    mongoose = require('mongoose');
 
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 var app=express();
 
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req,res){
+       Book.find(function(err,books){
+            if(err){
+                res.status(500).send(err);
+            }
+            else{
+                res.json(books);
+            }
+       });
+    });
+
+app.use('/api', bookRouter);
+
 
 app.get('/',function(req,res){
     res.send("welcome to my api");
 });
 
 app.listen(port, function(){
-    console.log('running on port:' + port);
+    console.log('Gulp is running my app on PORT:' + port);
 });
---
+++ models/bookModel.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alexandrutimofte on 1/5/2017.
+ */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema();
+
+var bookModel = new Schema({
+    title: {type: String},
+    author: {type: String},
+    genre: {type: String},
+    read: {type: Boolean, default:false}
+});
+
+module.exports = mongoose.model('Book', bookModel);
